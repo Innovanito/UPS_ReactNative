d88a9d23539b1236c91421ed0e076e786a815737
Innovanito <khg05311@naver.com>
2022-11-25 18:38:26 +0900

    1:30

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,5 +1,5 @@
 import { View, Text, SafeAreaView,ScrollView, ActivityIndicator } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import {useTailwind} from 'tailwind-rn/dist';
 import { CompositeNavigationProp,useNavigation } from '@react-navigation/native';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
@@ -7,6 +7,7 @@ import { TabStackParamList } from '../navigator/TabNavigator';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigator/RootNavigator';
 import {Image} from "@rneui/themed";
+import { Input } from '@rneui/base';
 
 
 export type CustomerScreenNavigationProp = CompositeNavigationProp<
@@ -17,6 +18,7 @@ export type CustomerScreenNavigationProp = CompositeNavigationProp<
 const CustomersScreen = () => {
   const tw = useTailwind();
   const navigation = useNavigation<CustomerScreenNavigationProp>()
+  const [input, setInput] = useState<string>("")
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -24,13 +26,19 @@ const CustomersScreen = () => {
     })
   })
 
-
   return (
-    <ScrollView>
+    <ScrollView style={{backgroundColor: '#59C1CC'}}>
       <Image
         source={{ uri: 'https://links.papareact.com/3jc'}}
         containerStyle={tw("h-64 w-full")}
         PlaceholderContent={<ActivityIndicator/>}
+      />
+
+      <Input
+        placeholder='Search by Customer'
+        value={input}
+        onChangeText={setInput}
+        containerStyle={tw('bg-white pt-5 pb-0 px-10')}
       />
     </ScrollView>
   )
